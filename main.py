a65b9e32f2f5fa720ca3ba301e88e0dc320dbc99
andrewaeva@ya.ru <123321223a>
2014-11-08 19:52:25 +0300

    Add all_legit.txt

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 __author__ = 'andrewa'
 #-*- coding: utf-8 -*-
 import numpy as np
-A = open('russian_words.txt', 'r').read().split('\n')
-B = []
-for i in A:
-    i = i.split("|")
-    B.append(i[3].strip())
-C = open('russian_words_final.txt', 'w')
-for i in B:
-    C.write(i+'\n')
-C.close()
+#A = open('russian_words.txt', 'r').read().split('\n')
+#B = []
+#for i in A:
+#    i = i.split("|")
+#    B.append(i[3].strip())
+#C = open('russian_words_final.txt', 'w')
+#for i in B:
+#    C.write(i+'\n')
+#C.close()
+
+######################################################
+
+#alexa = open('alexa.csv', 'r').read().split('\n')
+#alexa_domain = []
+#for i in alexa:
+#    i = i.split(',')
+#    try:
+#        alexa_domain.append(i[1])
+#    except:
+#        pass
+#opendns_random = open('opendns-random-domains.txt', 'r').read().split('\n')
+#opendns_top = open('opendns-top-domains.txt', 'r').read().split('\n')
+#opendns_random.extend(alexa_domain)
+#opendns_random.extend(opendns_top)
+#C = open('all_legit.txt', 'w')
+#C.seek(0)
+#for i in opendns_random:
+#    C.write(i+' 0\n')
+
+######################################################
