b3b5d0ca600bc5e612ce143c9e98cda6db99bcdb
Bruteforce <111>
2015-07-29 17:37:16 +0300

    update

--- DGAClassification.py
+++ DGAClassification.py
@@ -47,13 +47,30 @@ def entropy(s):
     p, lns = Counter(s), float(len(s))
     return -sum(count / lns * math.log(count / lns, 2) for count in p.values())
 
+
 all_domains['entropy'] = [entropy(x) for x in all_domains['domain']]
+
+# all_domains.boxplot('length','class')
+# pylab.ylabel('Domain Length')
+# all_domains.boxplot('entropy','class')
+# pylab.ylabel('Domain Entropy')
+
+# cond = all_domains['class'] != 'legit'
+# dga = all_domains[cond]
+# alexa = all_domains[~cond]
+# plt.scatter(alexa['length'], alexa['entropy'], s=140, c='r', label='Legit', alpha=.4)
+# plt.scatter(dga['length'], dga['entropy'], s=140, c='#aaaaff', label='DGA', alpha=.4)
+# plt.legend()
+# pylab.xlabel('Domain Length')
+# pylab.ylabel('Domain Entropy')
+# plt.show()
+
 
 alexa_vc = CountVectorizer(analyzer='char', ngram_range=(3, 5), min_df=1e-4, max_df=1.0)
 counts_matrix = alexa_vc.fit_transform(dataframe_dict['alexa']['domain'])
 alexa_counts = np.log10(counts_matrix.sum(axis=0).getA1())
 
-dict_vc = CountVectorizer(analyzer='char', ngram_range=(3,5), min_df=1e-5, max_df=1.0)
+dict_vc = CountVectorizer(analyzer='char', ngram_range=(3, 5), min_df=1e-5, max_df=1.0)
 counts_matrix = dict_vc.fit_transform(word_dataframe['word'])
 dict_counts = np.log10(counts_matrix.sum(axis=0).getA1())
 
@@ -61,23 +78,37 @@ all_domains['alexa_grams'] = alexa_counts * alexa_vc.transform(all_domains['doma
 all_domains['word_grams'] = dict_counts * dict_vc.transform(all_domains['domain']).T
 all_domains['diff'] = all_domains['alexa_grams'] - all_domains['word_grams']
 
-#cond = all_domains['class'] == 'conficker'
-#dga = all_domains[cond]
-#legit = all_domains[~cond]
-#plt.scatter(legit['length'], legit['alexa_grams'], s=120, c='#aaaaff', label='Alexa', alpha=.1)
-#plt.scatter(dga['length'], dga['alexa_grams'], s=40, c='r', label='DGA', alpha=.3)
-#plt.legend()
-#pylab.xlabel('Domain Length')
-#pylab.ylabel('Alexa NGram Matches')
+print 'Done data'
 
-#weird_cond = (all_domains['class']=='legit') & (all_domains['word_grams']<3) & (all_domains['alexa_grams']<2)
-#weird = all_domains[weird_cond]
-#all_domains.loc[weird_cond, 'class'] = 'weird'
-#not_weird = all_domains[all_domains['class'] != 'weird']
-#X = not_weird.as_matrix(['length', 'entropy', 'alexa_grams', 'word_grams'])
-#y = np.array(not_weird['class'].tolist())
 
-X = all_domains.as_matrix(['length', 'entropy', 'alexa_grams',  'word_grams', 'diff'])
+# cond = all_domains['class'] != 'legit'
+# dga = all_domains[cond]
+# legit = all_domains[~cond]
+# plt.scatter(legit['length'], legit['word_grams'],  s=140, c='r', label='legit', alpha=.1)
+# plt.scatter(dga['length'], dga['word_grams'], s=140, c='#aaaaff', label='DGA', alpha=.1)
+# plt.legend()
+# pylab.xlabel('Domain Length')
+# pylab.ylabel('Dictionary NGram Matches')
+# plt.show()
+
+# cond = all_domains['class'] != 'legit'
+# dga = all_domains[cond]
+# legit = all_domains[~cond]
+# plt.scatter(legit['length'], legit['diff'], s=140, c='r', label='Legit', alpha=.4)
+# plt.scatter(dga['length'], dga['diff'], s=140, c='#aaaaff', label='DGA', alpha=.4)
+# plt.legend()
+# pylab.xlabel('Domain Length')
+# pylab.ylabel('Diff')
+# plt.show()
+
+# weird_cond = (all_domains['class']=='legit') & (all_domains['word_grams']<3) & (all_domains['alexa_grams']<2)
+# weird = all_domains[weird_cond]
+# all_domains.loc[weird_cond, 'class'] = 'weird'
+# not_weird = all_domains[all_domains['class'] != 'weird']
+# X = not_weird.as_matrix(['length', 'entropy', 'alexa_grams', 'word_grams'])
+# y = np.array(not_weird['class'].tolist())
+
+X = all_domains.as_matrix(['length', 'entropy', 'alexa_grams', 'word_grams', 'diff'])
 y = np.array(all_domains['class'].tolist())
 
 from sklearn import cross_validation
@@ -87,59 +118,22 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 clf1 = LogisticRegression(random_state=1)
+clf1.fit(X_train, y_train)
 clf2 = RandomForestClassifier(random_state=1)
+clf2.fit(X_train, y_train)
 clf3 = GaussianNB()
+clf3.fit(X_train, y_train)
 clf4 = ExtraTreesClassifier()
+clf4.fit(X_train, y_train)
 
 eclf = VotingClassifier(estimators=[('lr', clf1), ('rf', clf2), ('gnb', clf3), ('etr', clf4)], voting='soft')
 
-clf1.fit(X, y)
-clf2.fit(X, y)
-clf3.fit(X, y)
-clf4.fit(X, y)
-eclf.fit(X, y)
-
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
-                     np.arange(y_min, y_max, 0.1))
-
-f, axarr = plt.subplots(2, 2, sharex='col', sharey='row', figsize=(10, 8))
-from itertools import product
-for idx, clf, tt in zip(product([0, 1], [0, 1]),
-                        [clf1, clf2, clf3, clf4, eclf],
-                        ['Regr', 'Foresr',
-                         'Bayes', 'Extra' 'Soft Voting']):
-
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-
-    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.4)
-    axarr[idx[0], idx[1]].scatter(X[:, 0], X[:, 1], c=y, alpha=0.8)
-    axarr[idx[0], idx[1]].set_title(tt)
-
-plt.show()
-
 for clf, label in zip([clf1, clf2, clf3, clf4, eclf], ['Logistic Regression', 'Random Forest', 'naive Bayes',
                                                        'Extra Tree', 'Ensemble']):
-    scores = cross_validation.cross_val_score(clf, X, y, cv=5, scoring='accuracy')
+    scores = cross_validation.cross_val_score(clf, X_test, y_test, cv=5, scoring='accuracy')
     print("Accuracy: %0.6f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
-print 'FOREST'
-
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-
-clf.fit(X, y)
-
-print 'VALIDATION'
-scores = sklearn.cross_validation.cross_val_score(clf, X, y, cv=5, n_jobs=4)
-print scores.mean()
-#all_domains.boxplot('entropy', 'class')
-
-
-#clf = sklearn.ensemble.RandomForestClassifier(n_estimators=20)
-#scores = sklearn.cross_validation.cross_val_score(clf, X, y, cv=5, n_jobs=4)
-#print scores.mean()
